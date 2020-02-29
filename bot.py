a690d6a5406326fa65efa1a74bd97de9e05909dc
larmoyant <61519655+larmoyant@users.noreply.github.com>
2020-02-29 00:39:26 -0600

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,13 +1,21 @@
 import discord
 
-client - discord.Client()
+id = client.get_guild(681917060011655179)
+
+
+client = discord.Client()
+
 
 @client.event
-async def on ready():
-	print("Zzzt! Meow! I'm Myaubot and I've officially connected to Nintendo WFC.")
-	
-@client.event
+async def on_message(message):
+    id = client.get_guild(id)
+    channels = ["animal-chatter"]
+    valid_users = ["larmoyant#4444"]
 
-client.run('NjgyMzgwMTA4NDQ2Njk1NDU1.Xlcalw.9A_LON7CKrtHom68FG0pa6OaiTY')
-	
-	
+    if str(message.channel) in channels and str(message.author) in valid_users:
+        if message.content.find("!hello") != -1:
+            await message.channel.send("*kzzrt* Hi there! Myaubot h-here! *krsh* C-can you hear me?") 
+        elif message.content == "!users":
+            await message.channel.send(f"""# of Members: {id.member_count}""")
+
+client.run(NjgyMzgwMTA4NDQ2Njk1NDU1.XlnnDw.nc3JsqwMIKvm0X9zx9QKJINg82w)
