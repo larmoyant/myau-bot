eae7f690dd4e99c8f52299ca0b8dab1cdeb33d63
larmoyant <61519655+larmoyant@users.noreply.github.com>
2020-02-28 21:09:18 -0600

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,13 @@
+import discord
+
+client - discord.Client()
+
+@client.event
+async def on ready():
+	print("Zzzt! Meow! I'm Myaubot and I've officially connected to Nintendo WFC.")
+	
+@client.event
+
+client.run('NjgyMzgwMTA4NDQ2Njk1NDU1.Xlcalw.9A_LON7CKrtHom68FG0pa6OaiTY')
+	
+	
